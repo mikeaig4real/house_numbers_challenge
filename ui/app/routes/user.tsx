4f08d7b30063be7d3c4d508f8cf6660380885eec
mikeaig4real <mikeaig4realonreal@gmail.com>
2025-06-13 23:47:16 +0100

    feat: implement user authentication check and redirect in User component

--- ui/app/routes/user.tsx
+++ ui/app/routes/user.tsx
@@ -1,6 +1,17 @@
-import { Outlet } from '@remix-run/react';
+import { Outlet, useNavigate } from '@remix-run/react';
+import { useAuth } from '../context/AuthContext';
+import { useEffect } from 'react';
 
-export default function User() {
+export default function User ()
+{
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate('/auth/signin', { replace: true });
+    }
+  }, [navigate, user]);
+  if (!user) return null;
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-[#1a0025]">
       <Outlet />
